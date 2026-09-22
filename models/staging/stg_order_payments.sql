92d555b0d4441d147483779f3876914ff5099df7
dabade-rajat <dabade.rajat@gmail.com>
2026-09-22 10:22:22 +0530

    solve: Stage the raw marketplace tables (#5)
    Merged by DZ0

---
+++ models/staging/stg_order_payments.sql
@@ -0,0 +1,7 @@
+select
+    cast(order_id as varchar)                as order_id,
+    cast(payment_sequential as integer)      as payment_sequential,
+    cast(payment_type as varchar)            as payment_type,
+    cast(payment_installments as integer)    as payment_installments,
+    cast(payment_value as decimal(10, 2))    as payment_value
+from {{ source('raw', 'olist_order_payments_dataset') }}
